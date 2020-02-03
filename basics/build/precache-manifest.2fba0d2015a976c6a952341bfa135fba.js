92baebb7a708855480fd5b5f5b20ad3d08f3947a
dhilt <d.alexanov@gmail.com>
2020-02-03 04:48:17 +0300

    basics build

---
+++ basics/build/precache-manifest.2fba0d2015a976c6a952341bfa135fba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "274f8e84f0d060fd6b08a4a658659968",
+    "url": "/index.html"
+  },
+  {
+    "revision": "653d9d2369a2b680817e",
+    "url": "/static/css/main.b427dff9.chunk.css"
+  },
+  {
+    "revision": "9f209b9122f6906250f8",
+    "url": "/static/js/2.f0121c8d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.f0121c8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "653d9d2369a2b680817e",
+    "url": "/static/js/main.5e29ccff.chunk.js"
+  },
+  {
+    "revision": "bc4df9a80617c213c747",
+    "url": "/static/js/runtime-main.3216c54d.js"
+  }
+]);
